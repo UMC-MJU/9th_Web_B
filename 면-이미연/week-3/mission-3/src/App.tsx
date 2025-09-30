80ab34f17afae31fbdaa6fed158e261e8f645505
miyeon04 <miyeoni241@gmail.com>
2025-09-30 23:23:31 +0900

    chore: 3주차 미션 초기 설정

--- 면-이미연/week-3/mission-3/src/App.tsx
+++ 면-이미연/week-3/mission-3/src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css';
+import HomePage from './pages/HomePage';
 import MoviePage from './pages/MoviePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage';
+import MovieDetailPage from './pages/MovieDetailPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: 'movies/:category',
+        element: <MoviePage />,
+      },
+      {
+        path: 'movies/:movieId',
+        element: <MovieDetailPage />,
+      }
+    ],
+  },
+]);
 
 function App() {
-  console.log(import.meta.env.VITE_TMDB_KEY);
-  return (
-    <>
-      <MoviePage />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
-
 
 export default App;
